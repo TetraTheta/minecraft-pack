97a43a07e9c55c0c7c66f7a3d4954dff2a366981
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2023-06-24 17:17:54 +0900

    update: Update build script
    7-Zip will ignore markdown files when archiving

--- .script/create_datapack.bat
+++ .script/create_datapack.bat
@@ -2,4 +2,4 @@
 @setlocal
 for %%A in ("%~dp0.") do set "parent=%%~dpA"
 pushd "%parent%"
-"C:\Program Files\7-Zip\7z.exe" a -tzip -mx=9 "custom-datapack.zip" "data\*" "pack.mcmeta"
+"C:\Program Files\7-Zip\7z.exe" a -tzip -mx9 -r -x!*.md "custom-datapack.zip" "data\*" "pack.mcmeta"
