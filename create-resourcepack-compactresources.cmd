0030cbc02adf0e3fe3ec103d4f2a71468d3b9eef
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2026-06-11 13:18:58 +0900

    chore: 제외 목록 이름 변경
    해당 파일은 데이터팩뿐만 아니라 리소스팩 제외 목록 역할도 함

--- create-resourcepack-compactresources.cmd
+++ create-resourcepack-compactresources.cmd
@@ -3,7 +3,7 @@ setlocal
 
 set "PATH=C:\Program Files\7-Zip-Zstandard;C:\Program Files\7-Zip;%PATH%"
 
-7za.exe a -tzip -mx9 "%~dp0\compact-resources-pack.zip" -xr@"%~dp0\exclude-data-pack.txt" "%~dp0\resource-pack\compact-resources-pack\*"
+7za.exe a -tzip -mx9 "%~dp0\compact-resources-pack.zip" -xr@"%~dp0\exclude.txt" "%~dp0\resource-pack\compact-resources-pack\*"
 
 endlocal
 pause
